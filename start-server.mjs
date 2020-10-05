1cf0daff799c23fe1b7efaaa600d6e276375eeca
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-10-06 00:32:15 +0200

    Update hititipi and improve security headers

--- start-server.mjs
+++ start-server.mjs
@@ -4,19 +4,30 @@ import { chainAll } from 'hititipi/src/middlewares/chain-all.js';
 import { chainUntilResponse } from 'hititipi/src/middlewares/chain-until-response.js';
 import { contentEncoding } from 'hititipi/src/middlewares/content-encoding.js';
 import { contentLength } from 'hititipi/src/middlewares/content-length.js';
+import { csp } from 'hititipi/src/middlewares/csp.js';
 import { hititipi } from 'hititipi';
+import { hsts, ONE_YEAR } from 'hititipi/src/middlewares/hsts.js';
 import { keepAlive } from 'hititipi/src/middlewares/keep-alive.js';
 import { logRequest } from 'hititipi/src/middlewares/log-request.js';
 import { notModified } from 'hititipi/src/middlewares/not-modified.js';
+import { permissionsPolicy } from 'hititipi/src/middlewares/permissions-policy.js';
+import { redirectHttps } from 'hititipi/src/middlewares/redirect-https.js';
 import { redirectNormalizedPath } from 'hititipi/src/middlewares/redirect-normalized-path.js';
 import { referrerPolicy } from 'hititipi/src/middlewares/referrer-policy.js';
 import { serverName } from 'hititipi/src/middlewares/server-name.js';
 import { socketId } from 'hititipi/src/middlewares/socket-id.js';
 import { staticFile } from 'hititipi/src/middlewares/static-file.js';
-import { xFrameOptions } from 'hititipi/src/middlewares/x-frame-options.js';
-import { csp } from 'hititipi/src/middlewares/csp.js';
+import { xContentTypeOptions } from 'hititipi/src/middlewares/x-content-type-options.js';
 
 const PORT = process.env.PORT || 8080;
+
+function ifProduction (middleware) {
+  return () => {
+    if (process.env.NODE_ENV === 'production') {
+      return middleware;
+    }
+  };
+}
 
 async function run () {
 
@@ -29,9 +40,12 @@ async function run () {
             socketId(),
             keepAlive({ max: 5, timeout: 100 }),
             referrerPolicy('same-origin'),
-            xFrameOptions('SAMEORIGIN'),
+            permissionsPolicy(),
+            xContentTypeOptions(),
             csp(),
+            ifProduction(hsts({ maxAge: ONE_YEAR, includeSubDomains: true })),
             chainUntilResponse([
+              ifProduction(redirectHttps()),
               redirectNormalizedPath(),
               staticFile({ root: '_site' }),
             ]),
