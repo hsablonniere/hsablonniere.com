37e54455102be26c0cac1f11131efbbc8d22c17e
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2020-10-18 02:05:18 +0200

    layouts: add edit link at the bottom of article

--- pages/_data/eleventyComputed.js
+++ pages/_data/eleventyComputed.js
@@ -4,4 +4,5 @@ const dtfShort = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short'
 module.exports = {
   formattedDate: (data) => dtfLong.format(data.page.date),
   shortDate: (data) => dtfShort.format(data.page.date),
+  githubEditUrl: (data) => new URL(data.page.inputPath, 'https://github.com/hsablonniere/hsablonniere.com/blob/master/').toString(),
 };
