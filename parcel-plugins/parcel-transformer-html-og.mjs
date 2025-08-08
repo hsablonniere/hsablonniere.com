1ca64c6a965c46f710f51da65fa0eef592ece4ec
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2025-08-08 15:56:34 +0200

    feat: re-enable OG image transformer

--- parcel-plugins/parcel-transformer-html-og.mjs
+++ parcel-plugins/parcel-transformer-html-og.mjs
@@ -3,27 +3,19 @@
 // Requirements:
 // * it needs to run after "@parcel/transformer-html" so it can work with IDs
 
-import path from 'path';
 import { createPostHtmlTransformer } from './common-posthtml-transformer.mjs';
-import { SOURCE_DIR } from './common.mjs';
 
 export default createPostHtmlTransformer(async (asset) => {
 
-  const assetDir = path.parse(asset.filePath).dir;
-
   function transformNode (node) {
 
-    const idForImageFullUrl = node.attrs.content;
-    const imageUrlString = asset.getDependencies().find((d) => d.id === idForImageFullUrl);
-    const imageUrl = new URL(imageUrlString.specifier);
-    const imageAbsolutePath = path.join(SOURCE_DIR, imageUrl.pathname);
-    const imageRelativePath = path.relative(assetDir, imageAbsolutePath);
+    const imageRelativePath = './opengraph.jpg';
 
     // Create a new URL dependency, with relative path, so it's linked to the image
     const idForRelativePath = asset.addURLDependency(imageRelativePath);
 
     // Rewrite the URL with the new relative path ID
-    node.attrs.content = imageUrl.origin + idForRelativePath;
+    node.attrs.content = 'https://www.hsablonniere.com' + idForRelativePath;
 
     return node;
   }
